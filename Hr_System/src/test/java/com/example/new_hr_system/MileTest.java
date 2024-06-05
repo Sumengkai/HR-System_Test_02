28e152e8a89b46dde9f098f650b530fad7628aca
Sumengkai <a0973038822@gmail.com>
2024-06-05 17:18:02 +0800

    aa

--- Hr_System/src/test/java/com/example/new_hr_system/MileTest.java
+++ Hr_System/src/test/java/com/example/new_hr_system/MileTest.java
@@ -9,19 +9,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 @SpringBootTest
 public class MileTest {
 
-	@Autowired
-	private JavaMailSender mailSender;
 
-	@Test
-	public void sendSimpleMail() {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("kennymax22581997@gmail.com");
-		// 多個收件人可以用ArrayList或HashSet
-		message.setTo("mickeychen8507@gmail.com", "a0973038822@gmail.com");
-		message.setSubject("主旨：測試測試");
-		message.setText("神奇逸翔");
-
-		mailSender.send(message);
-	}
 
 }
